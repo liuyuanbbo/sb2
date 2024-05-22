6d81dad6c3e83077e6ec44f248c313ca694a86fd
zjc@0027028800 <zheng.jianchao@iwhalecloud.com>
2024-05-22 10:10:45 +0800

    add module b

---
+++ micro-srv-b/src/main/java/org/zmz/b/MicroSrvB_App.java
@@ -0,0 +1,11 @@
+package org.zmz.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSrvB_App {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSrvB_App.class, args);
+    }
+}
