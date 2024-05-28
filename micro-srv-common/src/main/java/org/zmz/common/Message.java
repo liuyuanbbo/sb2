e6ed6579e2e9dcebcadcc51555bc86776773069a
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-05-28 16:23:09 +0800

    add toString()

--- micro-srv-common/src/main/java/org/zmz/common/Message.java
+++ micro-srv-common/src/main/java/org/zmz/common/Message.java
@@ -2,12 +2,14 @@ package org.zmz.common;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@ToString
 public class Message implements Serializable {
     private static final long serialVersionUID = 2275564594226992724L;
     // id
