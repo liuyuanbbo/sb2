8fe57241c633e942850368d8b8f110847233cf08
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-06-14 20:08:35 +0800

    模拟接口2

--- micro-srv-c/src/test/java/org/zmz/c/test/RegexTest.java
+++ micro-srv-c/src/test/java/org/zmz/c/test/RegexTest.java
@@ -1,10 +1,19 @@
 package org.zmz.c.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class RegexTest {
     @Test
-    public void t1(){
+    public void t1() {
+        boolean b1 = NumberUtils.isCreatable("123");
+        boolean b2 = NumberUtils.isCreatable("1.331");
+        boolean b3 = NumberUtils.isCreatable("71991L");
+        boolean b4 = NumberUtils.isCreatable("");
+        boolean b5 = NumberUtils.isCreatable("e1");
 
+        log.info("{} {} {} {} {}", b1, b2, b3, b4, b5);
     }
 }
