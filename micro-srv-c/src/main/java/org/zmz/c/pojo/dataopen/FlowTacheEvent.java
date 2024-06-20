8611c7dbea284181b1b22f9849a1124bdbfa44a2
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-06-20 19:30:10 +0800

    mybatis 逆向工程 2-6

---
+++ micro-srv-c/src/main/java/org/zmz/c/pojo/dataopen/FlowTacheEvent.java
@@ -0,0 +1,15 @@
+package org.zmz.c.pojo.dataopen;
+
+public class FlowTacheEvent {
+    private Long flowTacheId;
+
+    private String flowTypeId;
+
+    private String finishHandler;
+
+    private String failHandler;
+
+    private String state;
+
+    private String auditTacheType;
+}
