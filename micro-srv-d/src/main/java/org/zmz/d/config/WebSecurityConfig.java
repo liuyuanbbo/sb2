ac82f886723111e4788a786a4e9ed009b4f7a7b0
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-08-14 18:18:29 +0800

    测试 环境变量获取属性

--- micro-srv-d/src/main/java/org/zmz/d/config/WebSecurityConfig.java
+++ micro-srv-d/src/main/java/org/zmz/d/config/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package org.zmz.d.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 /**
  * @author Zmz
  */
+@Slf4j
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+    @Resource
+    private Environment env;
 
     @Value("${spring.security.basic.enabled:false}")
     private boolean enabled;
@@ -26,6 +35,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        String property = env.getProperty("aa.init.enabled");
+        if (property == null || "true".equals(property)) {
+            log.info(">>>>>>>>>>>>>>>>>>>>>测试>>>>>>>>>>>>>>>>>>>>");
         }
     }
 }
