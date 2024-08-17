cb3b8bdea7bdafff57d629ae8784a4065ba5ae92
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-08-17 11:25:29 +0800

    添加 pgsql mapper

---
+++ micro-srv-d/src/main/java/org/zmz/d/mapper/code01/pgsql/DepartmentsMapper.java
@@ -0,0 +1,10 @@
+package org.zmz.d.mapper.code01.pgsql;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.zmz.d.pojo.code01.pgsql.Departments;
+
+/**
+ * @author Zmz
+ */
+public interface DepartmentsMapper extends BaseMapper<Departments> {
+}
---
+++ micro-srv-d/src/main/java/org/zmz/d/mapper/code01/pgsql/EmployeesMapper.java
@@ -0,0 +1,10 @@
+package org.zmz.d.mapper.code01.pgsql;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.zmz.d.pojo.code01.pgsql.Employees;
+
+/**
+ * @author Zmz
+ */
+public interface EmployeesMapper extends BaseMapper<Employees> {
+}
