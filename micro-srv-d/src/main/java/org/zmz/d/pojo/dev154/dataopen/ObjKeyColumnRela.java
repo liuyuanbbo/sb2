5efcf34b44f5991ba059164286e701d4748ccbd7
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-08-21 20:53:23 +0800

    micro-srv-d 添加 数据指标服务

--- micro-srv-d/src/main/java/org/zmz/d/pojo/dev154/dataopen/ObjKeyColumnRela.java
+++ micro-srv-d/src/main/java/org/zmz/d/pojo/dev154/dataopen/ObjKeyColumnRela.java
@@ -1,6 +1,17 @@
 package org.zmz.d.pojo.dev154.dataopen;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@TableName(value = "obj_key_column_rela")
 public class ObjKeyColumnRela {
+    @TableId
     private Long relaId;
 
     private Long parentRelaId;
--- micro-srv-d/src/main/java/org/zmz/d/pojo/dev154/dataopen/ObjKeyTableRela.java
+++ micro-srv-d/src/main/java/org/zmz/d/pojo/dev154/dataopen/ObjKeyTableRela.java
@@ -1,6 +1,17 @@
 package org.zmz.d.pojo.dev154.dataopen;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@TableName(value = "obj_key_table_rela")
 public class ObjKeyTableRela {
+    @TableId
     private Long relaId;
 
     private Long objectId;
