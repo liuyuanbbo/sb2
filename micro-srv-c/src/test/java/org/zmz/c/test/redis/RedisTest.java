a809ceab1457af4c91107a588fe3277daa0d5d78
zzjjcc <zheng.jianchao@iwhalecloud.com>
2024-09-09 21:00:18 +0800

    测试 redis

--- micro-srv-c/src/test/java/org/zmz/c/test/redis/RedisTest.java
+++ micro-srv-c/src/test/java/org/zmz/c/test/redis/RedisTest.java
@@ -3,6 +3,7 @@ package org.zmz.c.test.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +15,17 @@ public class RedisTest {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @Test
     public void t1() {
-        stringRedisTemplate.opsForHash().put("test", "test", "test");
+        redisTemplate.opsForHash().put("User:Counter", "1024", "1");
+        Object o = redisTemplate.opsForHash().get("User:Counter", "1024");
+        log.info("{}", o);
+        stringRedisTemplate.opsForHash().put("User:Counter", "1024", "2");
+        Object os = stringRedisTemplate.opsForHash().get("User:Counter", "1024");
+        log.info("{}", os);
     }
 
 }
